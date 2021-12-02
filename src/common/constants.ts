f1fe0855ae17ce688c9f3c109fb28255611a1653
lassejaco <lasse.jacobsen@ethereum.org>
2021-12-02 14:57:23 +0100

    Clean up metatags

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,21 +1,5 @@
-export const TITLE = 'useWeb3.xyz'
-export const DESCRIPTION = 'A curated overview of the best and latest resources on Ethereum, blockchain and Web3 development.'
-export const SITE_URL = 'https://useweb3.xyz/'
+export const TITLE = 'Devconnect'
+export const DESCRIPTION = 'Devconnect description'
+export const SITE_URL = 'https://devconnect.org/'
 export const IMAGE_OG = `${SITE_URL}images/social.png`
-export const SOCIAL_HANDLE = '@useWeb3'
-export const DOMAIN = 'useweb3.xyz'
-
-export const DEFAULT_REVALIDATE_PERIOD = 3600
-
-export const JOBS_SINCE_LAST_UPDATED = 60
-export const JOBS_AMOUNT_PER_COMPANY = 5
-export const JOBS_FILTER = ['ethereum', 'web3', 'developer', 'engineer', 'hacker', 'evangelist', 
-    'software', 'fullstack', 'full-stack', 'full stack', 'blockchain', 'solidity', 'protocol', 'engineering', 'product', 'security']
-export const JOBS_GREENHOUSE = ['consensys', 'consensysinc', 'gnosis', 'openzeppelin', 'figment', '0x', 
-    'blockdaemon', 'setlabs', 'dydx', 'brave', 'dharma', 'thesis', 'matterlabs']
-export const JOBS_LEVER = ['aragon', 'centrifuge', 'protocol', 'compound-2', 'Uniswap', 'Mycelium', 
-    'chainlink', 'RabbitHoleStudios', 'coin-gecko', 'with-foundation', 'Rarible', 'defialliance',
-    'zerion', '3box', 'ArtBlocks', 'blocknative', 'connext-network', 'fuellabs', 'alchemy', 'offchainlabs', 'axiomzen',
-    'immutable', 'filecoin', 'optimism', 'TorusLabs', 'certik', 'trailofbits', 'obol-tech', 'quicknode']
-export const JOBS_BREEZY = ['the-graph', 'immunefi', 'superrare', 'centrifuge', 'celer-network', 'bloxstaking', 'hal-collective-inc']
-export const JOBS_WORKABLE = ['superfluid', 'walletconnect', 'doingud', 'web3labs', 'ankr', 'paraswap', 'argenthq']
+export const SOCIAL_HANDLE = '@EFDevcon'
